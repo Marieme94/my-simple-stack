020e8fbb119eb0a71220e3f9e846d479e9d30201
Marieme94 <mariemekeita2@gmail.com>
2018-11-07 21:23:04 +0100

    Exercice 3 sur mock object

--- src/test/java/deqo/mkei/mysimplestack/SimpleStackImpTest.java
+++ src/test/java/deqo/mkei/mysimplestack/SimpleStackImpTest.java
@@ -3,44 +3,51 @@ package deqo.mkei.mysimplestack;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 
 import java.time.Clock;
 
 import static org.junit.Assert.*;
+import static org.mockito.MockitoAnnotations.*;
+
 
 public class SimpleStackImpTest {
-    SimpleStack stack;
+    @Mock
+    SimpleStack mock;
 
     @Before
     public void setUp() throws Exception {
-        stack = new SimpleStackImp();
+        initMocks(this);
+        //stack = new SimpleStackImp();
         System.out.println("Je laisse une trace avant chaque test");
     }
 
     @Test
     public void isEmpty() throws Exception {
-        Assert.assertTrue(stack.isEmpty());
+        Assert.assertTrue(mock.isEmpty());
         String el = "el";
-        stack.push(new Item(el));
-        Assert.assertFalse(stack.isEmpty());
+        mock.push(new Item(el));
+        Assert.assertFalse(mock.isEmpty());
     }
 
     @Test
     public void getSize() throws Exception {
-        Assert.assertEquals(0, stack.getSize());
+        Assert.assertEquals(0, mock.getSize());
         String el = "el";
-        stack.push(new Item(el));
-        stack.push(new Item(el));
-        Assert.assertEquals(2, stack.getSize());
+        mock.push(new Item(el));
+        mock.push(new Item(el));
+        Assert.assertEquals(2, mock.getSize());
     }
 
     @Test
     public void push() throws Exception {
         Item ita = new Item(new Integer(8));
-        Assert.assertEquals(0, stack.getSize());
-        stack.push(ita);
-        Assert.assertEquals(1, stack.getSize());
-        Item o = stack.peek();
+        Assert.assertEquals(0, mock.getSize());
+        mock.push(ita);
+        Assert.assertEquals(1, mock.getSize());
+        Item o = mock.peek();
         Assert.assertTrue(o.getValue() instanceof Integer);
         Integer integer = (Integer)o.getValue();
         Assert.assertEquals(8, integer.intValue());
@@ -50,13 +57,10 @@ public class SimpleStackImpTest {
     @Test
     public void peek() throws Exception {
         Item ita = new Item(new Integer(8));
-        Assert.assertEquals(0, stack.getSize());
-        stack.push(ita);
-        Assert.assertEquals(1, stack.getSize());
-        Item o = stack.peek();
-        Assert.assertEquals(1, stack.getSize());
-        Assert.assertTrue(o.getValue() instanceof Integer);
-        Integer integer = (Integer)o.getValue();
+        Assert.assertEquals(0, mock.getSize());
+        mock.push(ita);
+        Assert.assertEquals(1, mock.getSize());
+        Integer integer = (Integer) ita.getValue();
         Assert.assertEquals(8, integer.intValue());
         Assert.assertEquals(ita.getValue(), integer);
     }
@@ -64,11 +68,11 @@ public class SimpleStackImpTest {
     @Test
     public void pop() throws Exception {
         Item ita = new Item(new Integer(8));
-        Assert.assertEquals(0, stack.getSize());
-        stack.push(ita);
-        Assert.assertEquals(1, stack.getSize());
-        Item o = stack.pop();
-        Assert.assertEquals(0, stack.getSize());
+        Assert.assertEquals(0, mock.getSize());
+        mock.push(ita);
+        Assert.assertEquals(1, mock.getSize());
+        Item o = mock.pop();
+        Assert.assertEquals(0, mock.getSize());
         Assert.assertTrue(o.getValue() instanceof Integer);
         Integer integer = (Integer)o.getValue();
         Assert.assertEquals(8, integer.intValue());
